4ab7665111a2b49827c46d26ed469dc2559ad84f
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-20 14:50:07 -0600

    Classwork

---
+++ index.js
@@ -0,0 +1,35 @@
+// Exercise 1
+// for(i= -3; i <= 8; i++)
+// {
+//     console.log(i)
+// }
+
+
+// // Exercise 2
+//  for (i = -5; i <= 5; i++)
+//   { 
+//     console.log(i)
+   
+//  }
+
+
+// Exercise 3
+// for(i = -50; i <=50 ; i = i + 2)
+// {
+//     console.log(i)
+// }
+
+// Exercise 4
+
+// let high_scores = [2112, 1024, 9000, 9001, 7200, 2600, 10000]
+
+
+// for(let high_scores = 0; high_scores <= 10000; high_scores++)
+// {
+//     console.log(score[high_scores]);    
+// }
+
+
+
+
+
